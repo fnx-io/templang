e9264e9bd532431cc5b1d965204179cbde262a79
Jiří Zůna <jiri@zunovi.cz>
2016-07-12 10:41:54 +0200

    MOD Enable codemirror for template editing

--- lib/src/app_component.dart
+++ lib/src/app_component.dart
@@ -1,11 +1,16 @@
 import 'package:angular2/core.dart';
 
 import 'package:templang/src/edn.dart' as edn;
+import 'dart:js' as js;
 
 @Component(
     selector: 'app',
     templateUrl: 'app_component.html')
-class AppComponent implements OnInit {
+class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild("txtarea") ElementRef textArea;
+
+  js.JsObject editor;
 
   String txtInput = """
 (blok a b
@@ -21,8 +26,14 @@ class AppComponent implements OnInit {
 
   Object root;
 
+  ChangeDetectorRef changes;
+
+
+  AppComponent(ChangeDetectorRef ref) {
+    this.changes = ref;
+  }
+
   void inputChanged(String text) {
-    print(text);
     try {
       root = edn.parse(text);
     } catch (e) {
@@ -30,9 +41,27 @@ class AppComponent implements OnInit {
     }
   }
 
-
   @override
   ngOnInit() {
     inputChanged(txtInput);
   }
+
+  @override
+  ngAfterViewInit() {
+    js.JsObject cm = js.context['CodeMirror'];
+
+    var opts = {'lineNumbers': true};
+    editor = cm.callMethod("fromTextArea", [textArea.nativeElement, new js.JsObject.jsify(opts)]);
+    var self = this;
+    var handler = (dynamic ed, dynamic change) {
+      var jsObject = new js.JsObject.fromBrowserObject(ed);
+      String value = jsObject.callMethod("getValue", []);
+      txtInput = value;
+      self.inputChanged(value);
+      changes.detectChanges();
+    };
+    editor.callMethod("on", ['change', js.allowInterop(handler)]);
+  }
+
+
 }
